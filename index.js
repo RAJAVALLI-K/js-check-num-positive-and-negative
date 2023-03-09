dd0715a9d01852b36c619875aada5910e443aaa4
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-03-09 20:53:19 +0530

    add js-check num positive and negative

---
+++ index.js
@@ -0,0 +1,10 @@
+var number=prompt('enter a number:');
+if(number>=1){
+    alert('number is positive');
+}
+else if(number==0){
+    alert('number is zero');
+}
+else{
+    alert('number is negative');
+}
